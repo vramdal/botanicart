bbdf05adc2e93e9a006ff4afff4e3ae4b7dc2227
Vidar S. Ramdal <vramdal@gmail.com>
2018-03-06 01:08:15 +0100

    Starter på galleri lightbox

--- botanicart.no-node/components/Gallery.js
+++ botanicart.no-node/components/Gallery.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import Link from "next/link";
 
 export default class Gallery extends Component {
 
@@ -22,11 +23,23 @@ export default class Gallery extends Component {
     render() {
         return (<ul className={classnames("gallery", this.props.className)} ref={el => this._root = el} style={this.props.style}>
             {this.props.frames.map((frame, idx) =>
-                <li className={classnames("gallery-image-holder", this.props.framesClassName)} key={"frame-" + idx}>
-                    {frame}
+                <li id={`frame-${frame.key}`} className={classnames("gallery-image-holder", this.props.framesClassName)} key={"frame-" + idx}
+                >
+                    <a href={`#frame-${frame.key}`} key={`link-${frame.key}`}>
+                        {frame}
+                    </a>
+                    <div className="gallery-image-fullsize-holder">
+                        <img src="https://lh3.googleusercontent.com/-UCNCsoIsbps/UDvqoWoZs_I/AAAAAAAARZM/rTTBnwD0rO4xXSFGexLVjxZj7FT0sYzbwCHMYBhgL/s800/gulteple.png"
+                             alt="Malus - gult eple"/>
+                        <br/>
+                        <span className="gallery-image-fullsize-caption">Malus - gult eple</span>
+                    </div>
                 </li>)}
             {this.props.children}
             <style jsx>{`
+            .gallery-image-holder:target {
+               border: 2px solid red !important;
+            }
             `}</style>
         </ul>);
     }
@@ -37,7 +50,8 @@ Gallery.propTypes = {
     children: PropTypes.any,
     className: PropTypes.string,
     framesClassName: PropTypes.string,
-    style: PropTypes.object
+    style: PropTypes.object,
+    onGalleryFrameSelect: PropTypes.func
 };
 
 Gallery.defaultProps = {
--- botanicart.no-node/pages/default-template.js
+++ botanicart.no-node/pages/default-template.js
@@ -7,29 +7,34 @@ import santiyClient from '../lib/sanity';
 import imageUrlBuilder from '@sanity/image-url'
 import Slideshow from '../components/Slideshow';
 import Head from "next/head";
-import Link from "next/link";
 import Gallery from "../components/Gallery";
 
 const builder = imageUrlBuilder(santiyClient);
 
-const serializers = {
-    types: {
-        code: props => (
-            <pre data-language={props.node.language}>
-                <code>{props.node.code}</code>
-            </pre>),
-        galleri: props => {
-            if (props.node.presentation === "slideshow") {
-                return (
-                    <Slideshow key={props.node._key}
-                               className="image-frame" slidesClassName={"forside-slide"} displayMs={5000}
-                               slides={props.node.bilder.map((bilde, idx) => (<React.Fragment>
-                                   <img key={bilde._id} src={builder.image(bilde.image).width(600).ignoreImageParams().fit("clip").url()}/>
-                                   <p className={"image-caption"}>{bilde.name}</p>
-                                   {bilde.latin && <p>{bilde.latin}</p>}
-                               </React.Fragment>))}>
 
-                        <style jsx>{`
+class Index extends React.Component {
+
+    serializers(){
+
+        return {
+            types: {
+                code: props => (
+                    <pre data-language={props.node.language}>
+                    <code>{props.node.code}</code>
+                </pre>),
+                galleri: props => {
+                    if (props.node.presentation === "slideshow") {
+                        return (
+                            <Slideshow key={props.node._key}
+                                       className="image-frame" slidesClassName={"forside-slide"} displayMs={5000}
+                                       slides={props.node.bilder.map((bilde, idx) => (
+                                           <React.Fragment>
+                                               <img key={bilde._id} src={builder.image(bilde.image).width(600).ignoreImageParams().fit("clip").url()}/>
+                                               <p className={"image-caption"}>{bilde.name}</p>
+                                               {bilde.latin && <p>{bilde.latin}</p>}
+                                           </React.Fragment>))}>
+
+                                <style jsx>{`
                   :global(.image-frame) {
                     margin-top: 1em;
                     border: 3px double #626262;
@@ -48,22 +53,20 @@ const serializers = {
                     font-size: 18pt;
                   }
                     `}
-                        </style>
-                    </Slideshow>
-                );
-            } else if (props.node.presentation === 'galleri') {
-                return (
-                    <Gallery className={'galleri'} framesClassName={'galleri-bilde'} key={props.node._key} frames={props.node.bilder.map((bilde, idx) => (
-                        <Link href={`#frame-${bilde.slug.current}`}>
-                            <a id={`frame-${bilde.slug.current}`}>
-                                <img key={bilde._id} src={builder.image(bilde.image).height(150).url()}/>
-                                <p className={"image-caption"}>{bilde.name}
-                                    <span className="latin">{bilde.latin && (` (${bilde.latin})`)}</span>
-                                </p>
-                            </a>
-                        </Link>
-                    ))}>
-                        <style jsx>{`
+                                </style>
+                            </Slideshow>
+                        );
+                    } else if (props.node.presentation === 'galleri') {
+                        return (
+                            <Gallery className={'galleri'} onGalleryFrameSelect={this.onGalleryFrameSelect.bind(this)} framesClassName={'galleri-bilde'} key={props.node._key} frames={props.node.bilder.map((bilde, idx) => (
+                                <React.Fragment key={bilde.slug.current}>
+                                    <img key={bilde._id} src={builder.image(bilde.image).height(150).url()}/>
+                                    <p className={"image-caption"}>{bilde.name}
+                                        <span className="latin">{bilde.latin && (` (${bilde.latin})`)}</span>
+                                    </p>
+                                </React.Fragment>
+                            ))}>
+                                <style jsx>{`
                   :global(.galleri) {
                     display: flex;
                     flex-direction: row;
@@ -96,29 +99,32 @@ const serializers = {
                   }
 
                     `}
-                        </style>
-                    </Gallery>
-                )
-            } else {
-                console.log("this.props = ", this.props);
-                throw new Error("Ukjent bildeliste-type: " + props.node.presentation);
+                                </style>
+                            </Gallery>
+                        )
+                    } else {
+                        console.log("this.props = ", this.props);
+                        throw new Error("Ukjent bildeliste-type: " + props.node.presentation);
+                    }
+                }
             }
         }
-    }
-};
-
-class Index extends React.Component {
+    };
 
 
     render() {
         return (<Layout menypunkter={this.props.menypunkter} aktivSideSlug={this.props.aktivSideSlug}>
             {this.props.pageContent.tekst && this.props.pageContent.tekst.map(tekstblokk => (
-                <BlockContent key={tekstblokk._key} blocks={tekstblokk} serializers={serializers}/>
+                <BlockContent key={tekstblokk._key} blocks={tekstblokk} serializers={this.serializers()}/>
             ))}
             <Head>
             </Head>
 
         </Layout>);
+    }
+
+    onGalleryFrameSelect(frame) {
+        console.log("Index.onGalleryFrameSelect", arguments);
     }
 
     componentDidMount() {
